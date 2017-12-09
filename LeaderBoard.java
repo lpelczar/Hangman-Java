4abdf23fa0dec7aa0d79b2f4bac71580fe2bb07c
lpelczar <lukepelczar@gmail.com>
2017-12-09 22:12:50 +0100

    Implement Score class and constructor of leaderboard

---
+++ LeaderBoard.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class LeaderBoard {
+
+    private ArrayList<Score> scores = new ArrayList<Score>();
+
+    LeaderBoard() {
+        this.scores = scores;
+    }
+}
---
+++ Score.java
@@ -0,0 +1,59 @@
+import java.util.Date;
+
+class Score {
+
+    private String name;
+    private Date date;
+    private double guessingTime;
+    private int guessingTries;
+    private String guessedWord;
+
+    Score(String name, Date date, double guessingTime, int guessingTries, String guessedWord) {
+        this.name = name;
+        this.date = date;
+        this.guessingTime = guessingTime;
+        this.guessingTries = guessingTries;
+        this.guessedWord = guessedWord;
+    }
+
+	/**
+	* Returns value of name
+	* @return
+	*/
+	public String getName() {
+		return name;
+	}
+
+	/**
+	* Returns value of date
+	* @return
+	*/
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	* Returns value of guessingTime
+	* @return
+	*/
+	public double getGuessingTime() {
+		return guessingTime;
+	}
+
+	/**
+	* Returns value of guessingTries
+	* @return
+	*/
+	public int getGuessingTries() {
+		return guessingTries;
+	}
+
+	/**
+	* Returns value of guessedWord
+	* @return
+	*/
+	public String getGuessedWord() {
+		return guessedWord;
+	}
+
+}
